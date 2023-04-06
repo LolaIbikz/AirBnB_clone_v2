31bba8e4a7aff0c444c05fc929ab01b52601496f
lolaibikz <ibikunle.oluwafunmilola@gmail.com>
2023-04-06 16:05:56 -0700

    task 2 corrected

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-""" Script that distributes an archive to your web servers using do_deploy"""
-
-from fabric.api import local
-from fabric.operations import run, put, sudo
+"""web server distribution"""
+from fabric.api import *
 import os.path
-from fabric.api import env
-env.hosts = ['35.185.103.0', '35.237.21.105']
+
+env.user = 'ubuntu'
+env.hosts = ["104.196.155.240", "34.74.146.120"]
+env.key_filename = "~/id_rsa"
 
 
 def do_deploy(archive_path):
-    if (os.path.isfile(archive_path) is False):
+    """distributes an archive to your web servers
+    """
+    if os.path.exists(archive_path) is False:
         return False
-
     try:
-        nconfig = archive_path.split("/")[-1]
-        ndir = ("/data/web_static/releases/" + nconfig.split(".")[0])
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(ndir))
-        run("sudo tar -xzf /tmp/{} -C {}".format(nconfig, ndir))
-        run("sudo rm /tmp/{}".format(nconfig))
-        run("sudo mv {}/web_static/* {}/".format(ndir, ndir))
-        run("sudo rm -rf {}/web_static".format(ndir))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(ndir))
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
     except:
         return False
