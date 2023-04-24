c56a9cd9947760b8a0c6ba113ff5dbb006a12ab3
lolaibikz <ibikunle.oluwafunmilola@gmail.com>
2023-04-24 13:32:11 -0700

    task 1 to3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,32 +1,37 @@
-#!/usr/bin/python3
 
+#!/usr/bin/python3
+"""Flask framework
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """return hello hbhb
+    """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """return HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cfun(text):
-    txt = text.replace('_', " ")
-    return "C {}". format(txt)
+def text(text):
+    """return text given"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
-    txt = text.replace('_', " ")
-    return "Python {}". format(txt)
+def display(text):
+    """display “Python ”, followed by the value of the text"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run()
